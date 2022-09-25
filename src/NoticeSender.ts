4ac3e8da6f4fae522ba6ea8787f05c9453419214
fanta-git <fantaji4957t@gmail.com>
2022-09-25 16:23:42 +0900

    応急処置のコードが間違っていたのを修正

--- src/NoticeSender.ts
+++ src/NoticeSender.ts
@@ -37,7 +37,7 @@ class songNoticer {
             try {
                 const { channelId, registeredList } = await userData.get(userId) ?? {};
                 if (registeredList === undefined) continue;
-                if (registeredList.songs.some(v => v.video_id === this.#songData.video_id)) continue;
+                if (registeredList.songs.every(v => v.video_id !== this.#songData.video_id)) continue;
                 if (channelId === undefined) continue;
                 const user = await this.#client.users.fetch(userId);
                 const channel = await this.#client.channels.fetch(channelId);
