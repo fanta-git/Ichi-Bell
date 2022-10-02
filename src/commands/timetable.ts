89c3f99cde68867b75b8cd7c0df67ec29067f106
fanta-git <fantaji4957t@gmail.com>
2022-10-02 17:50:36 +0900

    選曲履歴を表示した時、通知対象の曲を強調するように

--- src/commands/timetable.ts
+++ src/commands/timetable.ts
@@ -1,9 +1,11 @@
 import { ApplicationCommandOptionType, EmbedBuilder } from 'discord.js';
 import BookMaker from '../BookMaker';
+import { userData } from '../database';
 import { formatLastPlayed, subdivision } from '../embedsUtil';
 import getKiiteAPI from '../getKiiteAPI';
 import SlashCommand from '../SlashCommand';
 
+const LIMIT = 10;
 const OPTIONS = {
     LIMIT: 'limit'
 } as const;
@@ -22,17 +24,20 @@ const timetable: SlashCommand = {
         }
     ],
     execute: async (client, interaction) => {
-        const limit = interaction.options.getInteger(OPTIONS.LIMIT) ?? 10;
+        const limit = interaction.options.getInteger(OPTIONS.LIMIT) ?? LIMIT;
+
+        const { registeredList } = await userData.get(interaction.user.id) ?? {};
         const data = await getKiiteAPI('/api/cafe/timetable', { limit: 100 });
         const selectionIds = data.map(v => v.id);
         const rotates = await getKiiteAPI('/api/cafe/rotate_users', { ids: selectionIds.join(',') });
         const songLines = data.map((v, i) => {
             const played = i ? `[${formatLastPlayed(v.start_time)}]` : '**[ON AIR]**';
             const title = `[${v.title}](https://www.nicovideo.jp/watch/${v.video_id})`;
+            const registedUnder = registeredList?.songs.some(item => item.video_id === v.video_id) ? '__' : '';
             const newFav = `:heartpulse:${v.new_fav_user_ids?.length ?? 0}`;
             const rotate = `:arrows_counterclockwise:${rotates[v.id]?.length ?? 0}`;
 
-            return `**${i + 1}.**${title}\n└${played}${newFav}${rotate}`;
+            return `**${i + 1}.**${registedUnder}${title}${registedUnder}\n└${played}${newFav}${rotate}`;
         });
 
         const pages = subdivision(songLines, limit).map(v => new EmbedBuilder({
